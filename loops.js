5f5a582898a38a8974090e665ad8e9f3c3e1f525
Kyle Rudeen <krudeen@gmail.com>
2018-07-30 21:48:40 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -17,11 +17,13 @@ function whileLoop(number){
   return 'done';
 }
 
-function doWhileLoop(array){
+
 function maybeTrue(){
-  return Math.random() >= 0.5)  
+  return Math.random() >= 0.5) 
+}
+  function doWhileLoop(array){
+ 
   do {array.pop()}
-  while (
-  }
+  while (maybeTrue())
 
 }
